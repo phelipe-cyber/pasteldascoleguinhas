cc6b2f8ed071db01c449416d36d37c21c8e76037
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-03-10 10:05:05 -0300

    App salvar pedido

--- pdv/mvc/app/app_pedido.php
+++ pdv/mvc/app/app_pedido.php
@@ -1,114 +1,127 @@
 <?php
-  session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>App - Pedido</title>
 </head>
+
 <body>
 
-<link href="../common/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="../common/css/bootstrap.min.css" rel="stylesheet" />
   <form method="POST" action="../model/app_gravadb.php">
-<?php
-  
-   $categoria = $_GET['categoria'];
-   $mesa = $_GET['mesa'];
-   $cliente = $_GET['cliente'];
-   $id = $_GET['id'];
+    <?php
+
+    $categoria = $_GET['categoria'];
+    $mesa = $_GET['mesa'];
+    $cliente = $_GET['cliente'];
+    $id = $_GET['id'];
 
 
-   include_once "../model/conexao.php";
+    include_once "../model/conexao.php";
 
-   $tab_produtos = "SELECT * FROM produtos WHERE categoria = '$categoria'";
+    $tab_produtos = "SELECT * FROM produtos WHERE categoria = '$categoria'";
 
-   $produtos = mysqli_query($conn, $tab_produtos);
+    $produtos = mysqli_query($conn, $tab_produtos);
 
 
     $i = $_SESSION['loginapp'];
 
-    if($i == 1){
+    if ($i == 1) {
 
 
-   ?>
+    ?>
 
-<div class="row" style="background: #2d3339; height: 13%;">
+      <div class="row" style="background: #2d3339; height: 13%;">
 
-  <h3 class="mb-12 " style="background: #2d3339; width: 5%; " ></h3>
-  <a style="background: #2d3339; height: 100%; width: 23%; color: white; " type="button" href="app_categoria.php?id=<?php echo $id; ?>" class="btn btn-outline-light"><h4>voltar</h4></a>
-  <h3 class="mb-12 " style="background: #2d3339; width: 16%; " ></h3>
+        <h3 class="mb-12 " style="background: #2d3339; width: 5%; "></h3>
+        <a style="background: #2d3339; height: 100%; width: 23%; color: white; " type="button" href="app_categoria.php?id=<?php echo $id; ?>" class="btn btn-outline-light">
+          <h4>voltar</h4>
+        </a>
+        <h3 class="mb-12 " style="background: #2d3339; width: 16%; "></h3>
 
-  <h4  class="mb-12 text-center" style="color: white; width: 20%; ">Mesa <?php echo $id; ?></h4>
+        <h4 class="mb-12 text-center" style="color: white; width: 20%; ">Mesa <?php echo $id; ?></h4>
 
-<h3 class="mb-12 " style="background: #2d3339; width: 36%; " ></h3>
-
-
-</div>
-<div class="mb-12 " style=" height: 5%;" ></div>
-
-<input class="btn btn-outline-success" type="submit" name="enviar" value="Finalizar Pedido">
-
- <h3 class="col-lg-6 text-center" style="color: black;"><?php echo $categoria; ?></h3>
+        <h3 class="mb-12 " style="background: #2d3339; width: 36%; "></h3>
 
 
-  
-    <table class="table">
-      <thead>
-        <tr>
-          
-          <th class="col-lg-1 "><b>Nome</b> </th>
-          <th class="col-lg-1 "><b>Estq</b> </th>
-          <th class="col-lg-1 "><b>Preço Unitário</b> </th>
-          <th class="col-lg-1 "><b>Qtde.</b> </th>
-          <th class="col-lg-1 "><b>Obs.</b> </th>
-        </tr>
-      </thead>
+      </div>
+
+      <div class="row">
+        <h4 class="col-lg-7">
+          <label for="">Cliente:</label>
+          <input autofocus type="text" class="form-control" width="100%" height="100%" name="cliente" id="cliente" value="" required>
+        </h4>
+      </div>
+
+      <div class="mb-12 " style=" height: 5%;"></div>
+
+      <input class="btn btn-outline-success" type="submit" name="enviar" value="Finalizar Pedido">
+
+      <h3 class="col-lg-6 text-center" style="color: black;"><?php echo $categoria; ?></h3>
 
 
-<?php
-  $index = 0;
-    while($rows_produtos = mysqli_fetch_assoc($produtos)){?>
-      <tbody>
-        <tr>
-          
-          <td style="color: #ac4549;"><b><?php echo $rows_produtos['nome'];?></b></td>
-          
-          <td><?php echo $rows_produtos['estoque_atual'];?></td>
-          <td>R$ <?php echo $rows_produtos['preco_venda'];?></td>
-          <td>
 
-            <!-- <form method="GET" action="app_finalizar.php"> -->
-            <!-- <form method="GET" action="app_gravadb.php"> -->
+      <table class="table">
+        <thead>
+          <tr>
 
-              <input type="hidden" name="detalhes[<?= $index ?>][id_produto]" id="id_produto" value="<?php echo $rows_produtos['id']; ?>">
-              <input type="hidden" name="detalhes[<?= $index ?>][id]" id="id" value="<?php echo $id; ?>">
-              <input type="hidden" name="detalhes[<?= $index ?>][nome]" id="nome" value="<?php echo utf8_encode($rows_produtos['nome']); ?>">
-              <input type="hidden" name="detalhes[<?= $index ?>][mesa]" id="mesa" value="<?php echo $mesa; ?>">
-              <input type="hidden" name="detalhes[<?= $index ?>][preco]" id="preco" value="<?php echo $rows_produtos['preco_venda']; ?>">
-              <input type="hidden" name="detalhes[<?= $index ?>][cliente]" id="cliente" value="<?php echo $cliente; ?>">
-              <input type="hidden" name="detalhes[<?= $index ?>][categoria]" id="categoria" value="<?php echo $categoria; ?>">
-              <!-- <button type="submit" class="btn btn-danger btn-icon-split btn-sm" >+</button> -->
-
-              <input class="bg-gradient-success" value="+" type="button" onclick="this.parentNode.querySelector('input[type=number]').stepUp()"></input>    
-              <input class="bg-gradient-default text-center" style="width:50px;" name="detalhes[<?= $index ?>][quantidade]" min="0" maxlength="5" name="quantity" value="0" type="number">
-              <input class="bg-gradient-danger" value="-" type="button" onclick="this.parentNode.querySelector('input[type=number]').stepDown()"></input>
-              
-            </td>
-            <td>
-              <textarea name="detalhes[<?php echo $index ?>][observacoes]" class="form-control" id="observacoes"></textarea>
-              
-            </td>
-          </form>
-        </tr>
-      </tbody>
-   <?php $index++; } ?>
+            <th class="col-lg-1 "><b>Nome</b> </th>
+            <th class="col-lg-1 "><b>Estq</b> </th>
+            <th class="col-lg-1 "><b>Preço Unitário</b> </th>
+            <th class="col-lg-1 "><b>Qtde.</b> </th>
+            <th class="col-lg-1 "><b>Obs.</b> </th>
+          </tr>
+        </thead>
 
 
-    </table>
+        <?php
+        $index = 0;
+        while ($rows_produtos = mysqli_fetch_assoc($produtos)) { ?>
+          <tbody>
+            <tr>
+
+              <td style="color: #ac4549;"><b><?php echo $rows_produtos['nome']; ?></b></td>
+
+              <td><?php echo $rows_produtos['estoque_atual']; ?></td>
+              <td>R$ <?php echo $rows_produtos['preco_venda']; ?></td>
+              <td>
+
+                <!-- <form method="GET" action="app_finalizar.php"> -->
+                <!-- <form method="GET" action="app_gravadb.php"> -->
+
+                <input type="hidden" name="detalhes[<?= $index ?>][id_produto]" id="id_produto" value="<?php echo $rows_produtos['id']; ?>">
+                <input type="hidden" name="detalhes[<?= $index ?>][id]" id="id" value="<?php echo $id; ?>">
+                <input type="hidden" name="detalhes[<?= $index ?>][nome]" id="nome" value="<?php echo utf8_encode($rows_produtos['nome']); ?>">
+                <input type="hidden" name="detalhes[<?= $index ?>][mesa]" id="mesa" value="<?php echo "Mesa " . $mesa ?>">
+                <input type="hidden" name="detalhes[<?= $index ?>][preco]" id="preco" value="<?php echo $rows_produtos['preco_venda']; ?>">
+                <!-- <input type="hidden" name="detalhes[<?= $index ?>][cliente]" id="cliente" value="<?php echo $cliente; ?>"> -->
+                <input type="hidden" name="detalhes[<?= $index ?>][categoria]" id="categoria" value="<?php echo $categoria; ?>">
+                <!-- <button type="submit" class="btn btn-danger btn-icon-split btn-sm" >+</button> -->
+
+                <input class="bg-gradient-success" value="+" type="button" onclick="this.parentNode.querySelector('input[type=number]').stepUp()"></input>
+                <input class="bg-gradient-default text-center" style="width:50px;" name="detalhes[<?= $index ?>][quantidade]" min="0" maxlength="5" name="quantity" value="0" type="number">
+                <input class="bg-gradient-danger" value="-" type="button" onclick="this.parentNode.querySelector('input[type=number]').stepDown()"></input>
+
+              </td>
+              <td>
+                <textarea name="detalhes[<?php echo $index ?>][observacoes]" class="form-control" id="observacoes"></textarea>
+
+              </td>
+  </form>
+  </tr>
+  </tbody>
+<?php $index++;
+        } ?>
+
+
+</table>
 
 
 <!-- Extra large modal -->
@@ -121,20 +134,21 @@
 </div>
 
 
-<?php } else { 
-   ?>
-   <script>
-     window.location.href='app_login.php'
-   </script>
- <?php
-  // header('Location: app_login.php');
-}
-  ?>
+<?php } else {
+?>
+  <script>
+    window.location.href = 'app_login.php'
+  </script>
+<?php
+      // header('Location: app_login.php');
+    }
+?>
 
 
-  <script src="../common/js/jquery-3.3.1.slim.min.js" ></script>
-  <script src="../common/js/popper.min.js" ></script>
-  <script src="../common/js/bootstrap.min.js" ></script>
-  
+<script src="../common/js/jquery-3.3.1.slim.min.js"></script>
+<script src="../common/js/popper.min.js"></script>
+<script src="../common/js/bootstrap.min.js"></script>
+
 </body>
+
 </html>
--- pdv/mvc/model/app_gravadb.php
+++ pdv/mvc/model/app_gravadb.php
@@ -45,13 +45,14 @@ $detalhes = $_POST['detalhes'];
    };
    
    $numeropedido = $pedido;
-   
-
+   $user =  $_SESSION['user'];
+	$cliente = $_POST['cliente'];
 
 foreach ($detalhes as $detalhesPedidos) {
+
 	$nome = $detalhesPedidos['nome'];
-	$preco = $detalhesPedidos['preco'];
-	$cliente = $detalhesPedidos['cliente'];
+	$preco_venda = $detalhesPedidos['preco'];
+	// $cliente = $detalhesPedidos['cliente'];
 	$quantidade = $detalhesPedidos['quantidade'];
 	$observacoes = $detalhesPedidos['observacoes'];
 	$categoria = $detalhesPedidos['categoria'];
@@ -60,91 +61,20 @@ foreach ($detalhes as $detalhesPedidos) {
 	if ($quantidade == 0 )
 	  continue;
 
-	  print_r($detalhesPedidos);
-	  exit();
-	//   $insert_table = "INSERT INTO pedido (idmesa, produto, quantidade, hora_pedido, valor, observacao, usuario) VALUES ('$mesa', '$nome', '$quantidade', '$hora_pedido', '$preco', '$observacoes', '$usuarioid')";
-	//   $adiciona_pedido = mysqli_query($conn, $insert_table);
-	
 	echo $insert_table = "INSERT INTO pedido (numeropedido, delivery,cliente, idmesa, produto, quantidade, hora_pedido, valor, observacao, usuario, gorjeta) VALUES
-	('$numeropedido','','$cliente', '$id_mesa', '$pedido', '$quantidade', '$hora_pedido', '$preco_venda', '$observacoes', '$user', '' )";
-
-	
-	  $insert_table = "UPDATE mesas SET status = '2', nome = '$cliente' WHERE id_mesa = $mesa";
-	//   $adiciona_pedido = mysqli_query($conn, $insert_table);
+	 ('$numeropedido','','$cliente', '$id_mesa', '$nome', '$quantidade', '$hora_pedido', '$preco_venda', '$observacoes', '$user', '' )";
+  		$adiciona_pedido = mysqli_query($conn, $insert_table);
+	echo "<br>";
+	  $update_table = "UPDATE mesas SET status = '2', nome = '$cliente' WHERE id_mesa = $mesa";
+	//   $adiciona_pedido = mysqli_query($conn, $update_table);
 
 	// header("Location: ../app/app_mesas.php");
 
-	$conn->close();
+	// $conn->close();
 
 }
 
-exit();
-
-   if ($quantidade == '1/2') {
-   	$quantidade = 0.5;
-   }
-   if ($quantidade == '1/3') {
-   	$quantidade = 0.333;
-   }
-   if ($quantidade == '1/4') {
-   	$quantidade = 0.25;
-   } 
-   if (is_numeric($quantidade)) {
-
-
-		$insert_table = "INSERT INTO pedido (idmesa, produto, quantidade, hora_pedido, valor, observacao, usuario) VALUES ('$mesa', '$nome', '$quantidade', '$hora_pedido', '$preco', '$observacoes', '$usuarioid')";
-		$adiciona_pedido = mysqli_query($conn, $insert_table);
-
-		$insert_table = "UPDATE mesas SET status = '2', nome = '$cliente' WHERE id_mesa = $mesa";
-		$adiciona_pedido = mysqli_query($conn, $insert_table);
-
-		header("Location: ../app/app_mesas.php");
-
-		$conn->close();
-
-
-
-?>
-		<div class="row" style="background: #2d3339; height: 13%;">
-			<h3 class="mb-12 " style="background: #2d3339; width: 30%; " ></h3>
-		<h4 class="mb-12 text-center"><a  type="button" href="../app/app_finalizar.php?nome=<?php echo "$nome";?>&preco=<?php echo "$preco";?>&clienteh=&cliente=<?php echo "$cliente";?>&quantidade=<?php echo "$quantidade";?>&observacoes=<?php echo "$observacoes";?>&mesa=<?php echo "$mesa";?>&id=<?php echo "$mesa";?>&categoria=<?php echo "$categoria";?>&id_produto=0" class="btn btn-warning">voltar</a></h4>
-		<h3 class="mb-12 " style="background: #2d3339; width: 5%; " ></h3>
-		<h4 class="mb-12 text-center"><a  type="button" href="../app/app_finalizar.php?nome=<?php echo "$nome";?>&preco=<?php echo "$preco";?>&clienteh=&cliente=<?php echo "$cliente";?>&quantidade=<?php echo "$quantidade";?>&observacoes=<?php echo "$observacoes";?>&mesa=<?php echo "$mesa";?>&id=<?php echo "$mesa";?>&categoria=<?php echo "$categoria";?>&id_produto=0" class="btn btn-warning">voltar</a></h4>
-		</div>
-<?php
-
-
-   }else{?>
-		<div class="row" style="background: #2d3339; height: 13%;">
-
-			<h3 class="mb-12 " style="background: #2d3339; width: 5%; " ></h3>
-			<a style="background: #2d3339; height: 100%; width: 23%; color: white; " type="button" href="../app/app_finalizar.php?nome=<?php echo "$nome";?>&preco=<?php echo "$preco";?>&clienteh=&cliente=<?php echo "$cliente";?>&quantidade=<?php echo "$quantidade";?>&observacoes=<?php echo "$observacoes";?>&mesa=<?php echo "$mesa";?>&id=<?php echo "$mesa";?>&categoria=<?php echo "$categoria";?>&id_produto=0" class="btn btn-outline-light"><h4>voltar</h4></a>
-			<h3 class="mb-12 " style="background: #2d3339; width: 16%; " ></h3>
-
-			<h4  class="mb-12 text-center" style="color: white; width: 20%; ">Mesa</h4>
-
-			<h3 class="mb-12 " style="background: #2d3339; width: 36%; " ></h3>
-
-
-		</div>
-		<div class="mb-12 " style=" height: 5%;" ></div>
-
-		<div class="mb-12 " style=" height: 70%; background: black; border-radius: 5px;" >
-			<h2 class="mb-12 text-center" style="color: white;"> ATENÇÂO ! </h2>
-			<h4 class="mb-12 text-center" style="color: white;"> O Valor </h4>
-			<h4 class="mb-12 text-center" style="color: red;">"<?php echo "$quantidade";?>"</h4>
-			<h4 class="mb-12 text-center" style="color: white;">Não é um número Válido,<br> Verifique o campo Quantidade  </h4>
-			<h4 class="mb-12 text-center" style="color: white;"> e tente Novamente ! </h4>
-			<h4 class="mb-12 text-center"><a  type="button" href="../app/app_finalizar.php?nome=<?php echo "$nome";?>&preco=<?php echo "$preco";?>&clienteh=&cliente=<?php echo "$cliente";?>&quantidade=<?php echo "$quantidade";?>&observacoes=<?php echo "$observacoes";?>&mesa=<?php echo "$mesa";?>&id=<?php echo "$mesa";?>&categoria=<?php echo "$categoria";?>&id_produto=0" class="btn btn-warning">voltar</a></h4>
-		</div>
-
-<?php
-
-   }
-
-?>
-
-
+ ?>
 
 
   <script src="../common/js/jquery-3.3.1.slim.min.js" ></script>
