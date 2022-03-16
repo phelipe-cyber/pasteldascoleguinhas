7d2e5275fdf6aa9139bc31ecdea4586297e9c9ff
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-03-15 22:05:16 -0300

    Update app_gravadb.php

--- pdv/mvc/model/app_gravadb.php
+++ pdv/mvc/model/app_gravadb.php
@@ -23,16 +23,36 @@ $hora_pedido = date('H:i');
 //    $mesa = $_GET['mesa'];
 $usuarioid = $_SESSION['usuarioid'];
 
- $numeropedido = $_POST['numeropedido'];
+$numeropedido = $_POST['numeropedido'];
 
-if($numeropedido == "" ){
+if ($numeropedido == "") {
 
-	echo "Pedido Vazio: " . $numeropedido;
-
-	   exit();
 
 	$user =  $_SESSION['user'];
 	$cliente = $_POST['cliente'];
+
+	$result_usuarios = ("SELECT MAX(numeropedido) as 'Pedido'FROM `pedido` ORDER BY numeropedido DESC limit 1 ");
+	$recebidos = mysqli_query($conn, $result_usuarios);
+
+	while ($row_usuario = mysqli_fetch_assoc($recebidos)) {
+
+		$novo_pedido = $row_usuario['Pedido'];
+	}
+	if ($novo_pedido == null) {
+		$novo_pedido = "1001";
+	} else {
+
+		$result_usuarios = ("SELECT MAX(numeropedido)+1 as 'Pedido'FROM `pedido` ORDER BY numeropedido DESC limit 1 ");
+		$recebidos = mysqli_query($conn, $result_usuarios);
+
+		while ($row_usuario = mysqli_fetch_assoc($recebidos)) {
+
+			$novo_pedido = $row_usuario['Pedido'];
+		}
+	};
+
+	$numeropedido = $novo_pedido;
+
 
 	foreach ($detalhes as $detalhesPedidos) {
 
@@ -62,18 +82,18 @@ if($numeropedido == "" ){
 
 
 	exit();
-}else{
+} else {
 
 
 	echo "Pedido: " . $numeropedido;
-	
-	   exit();
-	
+
+	exit();
+
 	$user =  $_SESSION['user'];
 	$cliente = $_POST['cliente'];
-	
+
 	foreach ($detalhes as $detalhesPedidos) {
-	
+
 		$nome = $detalhesPedidos['nome'];
 		$id_mesa = $detalhesPedidos['id'];
 		$preco_venda = $detalhesPedidos['preco'];
@@ -82,24 +102,23 @@ if($numeropedido == "" ){
 		$observacoes = $detalhesPedidos['observacoes'];
 		$categoria = $detalhesPedidos['categoria'];
 		$mesa = $detalhesPedidos['mesa'];
-	
+
 		if ($quantidade == 0)
 			continue;
-	
+
 		$insert_table = "INSERT INTO pedido (numeropedido, delivery,cliente, idmesa, produto, quantidade, hora_pedido, valor, observacao, usuario, gorjeta, `status`) VALUES
 	 ('$numeropedido','','$cliente', '$id_mesa', '$nome', '$quantidade', '$hora_pedido', '$preco_venda', '$observacoes', '$user', '', 2 )";
 		$adiciona_pedido = mysqli_query($conn, $insert_table);
 	}
-	
+
 	$update_table = "UPDATE mesas SET status = '2', nome = '$cliente' WHERE id_mesa = '$id_mesa' ";
 	$update_table = mysqli_query($conn, $update_table);
-	
+
 	echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=../app/app_mesas.php'>";
-	
+
 	$conn->close();
-	
 };
-	?>
+?>
 
 
 
