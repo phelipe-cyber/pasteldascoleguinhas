73ce1ba7b96f6d6229485c8981eff8568512c00f
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-05-31 17:23:50 -0300

    Update conexao.php

--- pdv/mvc/model/conexao.php
+++ pdv/mvc/model/conexao.php
@@ -1,18 +1,18 @@
 <?php
 	
-	$servidor = "db";
-	$usuario = "root";
-	$senha = "#tr0caf0ne#";
-	$dbname = "pdv";
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+	// $servidor = "db";
+	// $usuario = "root";
+	// $senha = "#tr0caf0ne#";
+	// $dbname = "pdv";
+	// $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
 
-// define('HOST', '185.214.126.10');
-// define('HOST', 'localhost');
-// define('USUARIO', 'u841971040_root');
-// define('SENHA', 'sC7MH#Sl9@');
-// define('DB', 'u841971040_pdv');
-// $conn = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+define('HOST', '185.214.126.10');
+define('HOST', 'localhost');
+define('USUARIO', 'u841971040_root');
+define('SENHA', 'sC7MH#Sl9@');
+define('DB', 'u841971040_pdv');
+$conn = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 
 
 ?>
