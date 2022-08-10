1b49a16fe2077ebc434b9772375af436cdcd0335
phelipe-cyber <61544699+phelipe-cyber@users.noreply.github.com>
2022-08-10 16:13:15 -0300

    Update apagar_previa.php

--- pdv/mvc/model/apagar_previa.php
+++ pdv/mvc/model/apagar_previa.php
@@ -2,7 +2,7 @@
 session_start();
 include_once("conexao.php");
 
-$apagar = "TRUNCATE TABLE `pdv`.`pedido_previa`";
+$apagar = "TRUNCATE TABLE `pedido_previa`";
 
 $adiciona_pedido = mysqli_query($conn, $apagar);
 
